ba2aa1413b4c2c3d6553594e65e5e0417540dd2a
Bartosz Trusiński <bartosztrusinski@gmail.com>
2023-04-20 16:59:58 +0200

    basic project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <h1 className='m-2 border-2 border-gray-300 p-3  text-center text-4xl font-bold text-teal-600 shadow-md'>
+      Hello world!
+    </h1>
+  );
+};
+
+export default App;
